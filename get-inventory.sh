3d3022f9c0874f0c163a11411b97f76e2f9c485d
Zack Cerza <zack@redhat.com>
2018-07-25 14:48:08 -0600

    Use convert-inventory.py

--- get-inventory.sh
+++ get-inventory.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -ex
 
 TRIPLEO_INVENTORY=tripleo-inventory.yaml
-CONVERT_PLAYBOOK=convert-inventory.yaml
 
 tripleo-ansible-inventory --static-yaml-inventory $TRIPLEO_INVENTORY
 ansible --ssh-extra-args "-o StrictHostKeyChecking=no" -i $TRIPLEO_INVENTORY all -m ping
-ansible-playbook -i ./$TRIPLEO_INVENTORY $CONVERT_PLAYBOOK
+./convert-inventory.py $TRIPLEO_INVENTORY | tee inventory
