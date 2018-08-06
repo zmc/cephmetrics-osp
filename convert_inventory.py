f219051fc99f2c65570ae0cd12e1f52edbd5472c
Zack Cerza <zack@redhat.com>
2018-08-06 11:49:39 -0700

    convert_inventory.py: rework
    * Add 'yaml' and 'raw' output formats
    * Drop INI-style output format in favor of YAML, allowing us to embed
      group_vars
    * Add our inventory format as an additional input format

--- convert_inventory.py
+++ convert_inventory.py
@@ -3,12 +3,6 @@ import argparse
 import json
 import yaml
 
-GROUP_MAP = dict(
-  Controller=['mons', 'mgrs'],
-  CephStorage=['osds'],
-  Compute=['iscsis'],
-)
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,9 +11,14 @@ def parse_args():
         nargs=1,
     )
     parser.add_argument(
+        "-i", "--input-format",
+        choices=["tripleo", "ceph-ansible"],
+        default="tripleo",
+    )
+    parser.add_argument(
         "-f", "--format",
-        choices=["ansible", "hosts", "hosts_yaml"],
-        default="ansible",
+        choices=["hosts", "hosts_yaml", "raw", "yaml"],
+        default="yaml",
     )
     return parser.parse_args()
 
@@ -38,8 +37,15 @@ def load_file(path):
         raise NotImplementedError
 
 
-def get_inventory(orig_obj, group_map):
-    new_obj = dict()
+def simplify_tripleo_inventory(orig_obj):
+    group_map = dict(
+      Controller=['mons', 'mgrs'],
+      CephStorage=['osds'],
+      Compute=['iscsis'],
+    )
+    new_obj = dict(
+        ungrouped=dict(),
+    )
     for orig_group_name, new_group_names in group_map.items():
         orig_group = orig_obj.get(orig_group_name)
         if orig_group is None:
@@ -47,35 +53,22 @@ def get_inventory(orig_obj, group_map):
         ips = dict()
         for child_name in orig_group['children'].keys():
             ips[child_name] = orig_obj[child_name]['vars']['ctlplane_ip']
-            for new_group_name in new_group_names:
-                if new_group_name not in new_obj:
-                    new_obj[new_group_name] = dict()
-                new_obj[new_group_name][child_name] = dict(
-                    ansible_host=ips[child_name],
-                    ansible_ssh_user='heat-admin',
-                )
-    return new_obj
-
-
-def format_ansible(inventory_obj):
-    lines = []
-    for group_name, group in inventory_obj.items():
-        lines.append('[%s]' % group_name)
-        for child_name in sorted(group.keys()):
-            s = ' '.join(
-                [child_name] +
-                ['%s=%s' % kv for kv in group[child_name].items()]
+            new_obj['ungrouped'][child_name] = dict(
+                ansible_host=ips[child_name],
+                ansible_ssh_user='heat-admin',
             )
-            lines.append(s)
-    return '\n'.join(lines)
+            for new_group_name in new_group_names:
+                new_obj.setdefault(new_group_name, dict()).setdefault(
+                    'hosts', dict())[child_name] = dict()
+    return new_obj
 
 
 def format_hosts_dict(inventory_obj):
     hosts_obj = dict()
-    for group in inventory_obj.values():
-        for host_name in group.keys():
-            if host_name not in hosts_obj:
-                hosts_obj[host_name] = group[host_name]['ansible_host']
+    for group_name, group in inventory_obj.items():
+        for host_name, host in group.items():
+            if 'ansible_host' in host:
+                hosts_obj[host_name] = host['ansible_host']
     return hosts_obj
 
 
@@ -103,10 +96,16 @@ if __name__ == "__main__":
 
     orig_obj = load_file(args.file[0])
 
-    inv = get_inventory(orig_obj, GROUP_MAP)
-    if args.format == "ansible":
-        print format_ansible(inv)
+    if args.input_format == "tripleo":
+        inv = simplify_tripleo_inventory(orig_obj)
+    elif args.input_format == "ceph-ansible":
+        inv = orig_obj
+
+    if args.format == "yaml":
+        print yaml.safe_dump(inv, default_flow_style=False)
     elif args.format == "hosts":
         print format_hosts(inv)
     elif args.format == "hosts_yaml":
         print format_hosts_yaml(inv)
+    elif args.format == "raw":
+        print inv
